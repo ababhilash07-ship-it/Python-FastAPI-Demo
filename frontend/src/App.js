63d1685b9b03b0d6e96965041ed203f1f586ee4b
Abhilash N <ab.abhilash07@gmail.com>
2025-09-09 14:35:32 +0530

    Api change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://python-fastapi-demo.onrender.com/api",
 });
 
 function App() {
